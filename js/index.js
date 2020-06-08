0c595ec22c1832f454b877e7bc61409f938d6400
Cade Scroggins <git@cade.me>
2020-06-08 14:35:53 -0700

    prettier

--- js/index.js
+++ js/index.js
@@ -1,113 +1,46 @@
-'use strict';
+"use strict";
 
-(function() {
-  smoothScroll.init({
-    speed: 500,
-    easing: 'easeInOutCubic',
-    offset: 0,
-    updateURL: true
-  });
-})();
-
-(function() {
+(function () {
   var slider = new IdealImageSlider.Slider({
-    selector: '#js-slider',
+    selector: "#js-slider",
     height: 250,
     interval: 5000,
-    transitionDuration: 300
+    transitionDuration: 300,
   });
 
   slider.start();
 })();
 
-(function() {
-  var modals = document.getElementsByClassName('modal');
-  var modalToggles = document.getElementsByClassName('modal-toggle');
+(function () {
+  var modals = document.getElementsByClassName("modal");
+  var modalToggles = document.getElementsByClassName("modal-toggle");
 
   for (var i = 0; i < modalToggles.length; i++) {
-    modalToggles[i].addEventListener('click', function(e) {
-      var id = 'js-modal-' + e.target.getAttribute('data-modal');
-      var modal = document.getElementById(id);
+    modalToggles[i].addEventListener(
+      "click",
+      function (e) {
+        var id = "js-modal-" + e.target.getAttribute("data-modal");
+        var modal = document.getElementById(id);
 
-      if (modal.classList.contains('visible')) {
-        modal.classList.remove('visible');
-      } else {
-        modal.classList.add('visible');
-      }
-    }, false);
+        if (modal.classList.contains("visible")) {
+          modal.classList.remove("visible");
+        } else {
+          modal.classList.add("visible");
+        }
+      },
+      false
+    );
   }
 
   for (var i = 0; i < modals.length; i++) {
-    modals[i].addEventListener('mousedown', function(e) {
-      if (e.target.classList.contains('modal')) {
-        e.target.classList.remove('visible');
-      }
-    }, false);
+    modals[i].addEventListener(
+      "mousedown",
+      function (e) {
+        if (e.target.classList.contains("modal")) {
+          e.target.classList.remove("visible");
+        }
+      },
+      false
+    );
   }
-})();
-
-(function() {
-  new MultiDatePicker({
-    dates: [ 'Jul 9, 2018', 'Jul 10, 2018', 'Jul 11, 2018', 'Jul 12, 2018', 'Jul 13, 2018' ],
-    align: 'left',
-    marginSides: '1.6rem',
-    marginTop: '87px'
-  });
-})();
-
-(function() {
-  var form = document.getElementById('js-form');
-  var formSubmitButton = document.getElementById('js-form-submit-button');
-  var submitText = formSubmitButton.innerHTML;
-
-  form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    formSubmitButton.innerHTML = 'Registering...';
-    formSubmitButton.disabled = true;
-
-    var inputs = form.getElementsByTagName('input');
-    var script = document.createElement('script');
-    var callback = 'jsonp_callback_' + Math.round(10000 * Math.random());
-    var params = '';
-
-    for (var i = 0; i < inputs.length; i++) {
-      var e = inputs[i];
-      var name = e.name;
-      var value = false;
-
-      switch (e.type) {
-        case 'checkbox':
-          value = e.checked;
-          break;
-        case 'radio':
-          if (e.checked) value = e.value;
-          break;
-        default:
-          value = e.value ? e.value : '';
-      }
-
-      if (value !== false) {
-        params += '&' + encodeURIComponent(name) + '=' + encodeURIComponent(value);
-      }
-    }
-
-    window[callback] = function(data) {
-      formSubmitButton.type = 'reset';
-      formSubmitButton.innerHTML = 'Success';
-      formSubmitButton.classList.add('success');
-      formSubmitButton.disabled = false;
-
-      delete window[callback];
-      document.body.removeChild(script);
-    };
-
-    script.src = form.action + '?callback=' + callback + params;
-    document.body.appendChild(script);
-  }, false);
-
-  form.addEventListener('reset', function(e) {
-    formSubmitButton.type = 'submit';
-    formSubmitButton.innerHTML = submitText;
-    formSubmitButton.classList.remove('success');
-  }, false);
 })();
--- spreadsheet.gs
+++ spreadsheet.gs
@@ -2,7 +2,7 @@ var SCRIPT_PROP = PropertiesService.getScriptProperties();
 
 function setup() {
   var doc = SpreadsheetApp.getActiveSpreadsheet();
-  SCRIPT_PROP.setProperty('key', doc.getId());
+  SCRIPT_PROP.setProperty("key", doc.getId());
 }
 
 function doGet(e) {
@@ -10,40 +10,44 @@ function doGet(e) {
     Logger.log(e);
 
     MailApp.sendEmail(
-      e.parameters['Email Address'],
-      'Camp Registration Confirmation',
-      'Hi, ' + e.parameters['First Name'] + '!\n\n' +
-        'Thanks for signing up for camp!\n' +
-        'Feel free to reply to this email with any questions.\n\n' +
-        'See you soon,\n' +
-        'The Willy Camp Team',
-      { name: 'Willy Camp' }
+      e.parameters["Email Address"],
+      "Camp Registration Confirmation",
+      "Hi, " +
+        e.parameters["First Name"] +
+        "!\n\n" +
+        "Thanks for signing up for camp!\n" +
+        "Feel free to reply to this email with any questions.\n\n" +
+        "See you soon,\n" +
+        "The Willy Camp Team",
+      { name: "Willy Camp" }
     );
 
     MailApp.sendEmail(
-      'awillygoodcamp@gmail.com',
-      'New Willy Camp Registration',
-      e.parameters['First Name'] + ' ' + e.parameters['Last Name'] +
-        ' just registered. Check out all registrations below.\n\n' +
-        'https://docs.google.com/spreadsheets/d/1A15LqjGThBQRdqg9glpWpk73beUR9D3Kr-cwln0XXUQ/'
+      "awillygoodcamp@gmail.com",
+      "New Willy Camp Registration",
+      e.parameters["First Name"] +
+        " " +
+        e.parameters["Last Name"] +
+        " just registered. Check out all registrations below.\n\n" +
+        "https://docs.google.com/spreadsheets/d/1A15LqjGThBQRdqg9glpWpk73beUR9D3Kr-cwln0XXUQ/"
     );
 
     recordData(e);
 
     var data = JSON.stringify({
-      'result': 'success',
-      'data': JSON.stringify(e.parameters)
+      result: "success",
+      data: JSON.stringify(e.parameters),
     });
 
     return ContentService.createTextOutput(
       e.parameter.callback + "(" + data + ")"
     ).setMimeType(ContentService.MimeType.JAVASCRIPT);
-  } catch(error) {
+  } catch (error) {
     Logger.log(error);
 
     var data = JSON.stringify({
-      'result': 'error',
-      'error': e
+      result: "error",
+      error: e,
     });
 
     return ContentService.createTextOutput(
@@ -56,8 +60,8 @@ function recordData(e) {
   Logger.log(JSON.stringify(e));
 
   try {
-    var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty('key'));
-    var sheet = doc.getSheetByName('responses');
+    var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty("key"));
+    var sheet = doc.getSheetByName("responses");
     var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
     var nextRow = sheet.getLastRow() + 1;
     var row = [new Date()];
@@ -65,12 +69,12 @@ function recordData(e) {
     for (var i = 1; i < headers.length; i++) {
       if (headers[i].length > 0) {
         var value = e.parameter[headers[i]];
-        row.push(value ? value : '');
+        row.push(value ? value : "");
       }
     }
 
     sheet.getRange(nextRow, 1, 1, row.length).setValues([row]);
-  } catch(error) {
+  } catch (error) {
     Logger.log(e);
   } finally {
     return;
