60d1e3efe17fa676c92a1fabc00b10cf0e611f4d
Cade Scroggins <git@cade.me>
2021-03-02 10:34:27 +0530

    2021 updates

--- js/index.js
+++ js/index.js
@@ -10,37 +10,3 @@
 
   slider.start();
 })();
-
-(function () {
-  var modals = document.getElementsByClassName("modal");
-  var modalToggles = document.getElementsByClassName("modal-toggle");
-
-  for (var i = 0; i < modalToggles.length; i++) {
-    modalToggles[i].addEventListener(
-      "click",
-      function (e) {
-        var id = "js-modal-" + e.target.getAttribute("data-modal");
-        var modal = document.getElementById(id);
-
-        if (modal.classList.contains("visible")) {
-          modal.classList.remove("visible");
-        } else {
-          modal.classList.add("visible");
-        }
-      },
-      false
-    );
-  }
-
-  for (var i = 0; i < modals.length; i++) {
-    modals[i].addEventListener(
-      "mousedown",
-      function (e) {
-        if (e.target.classList.contains("modal")) {
-          e.target.classList.remove("visible");
-        }
-      },
-      false
-    );
-  }
-})();
